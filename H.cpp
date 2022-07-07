79c1ec511950c1b8e9fae05de3f3ad7c721191cd
Nakaishi Ryosuke <acna2676@gmail.com>
2022-07-07 18:10:48 +0900

    ntoe

--- H.cpp
+++ H.cpp
@@ -5,7 +5,7 @@ int main() {
     // 変数a,b,cにtrueまたはfalseを代入してAtCoderと出力されるようにする。
     bool a = true;  // true または false
     bool b = false; // true または false
-    bool c = true;  // true または false
+    bool c = true;  // true または falsegit
 
     // ここから先は変更しないこと
 
