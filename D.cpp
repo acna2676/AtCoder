f910bafeb708d0da5136f7c1669dc1a4b401032e
Nakaishi Ryosuke <acna2676@gmail.com>
2022-08-13 10:26:40 +0900

    note: change directory struct

--- D.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << 1.0 / 2 * 100 * (100 + 1) << endl; // 2
-}
--- README.md
+++
@@ -1,5 +0,0 @@
-# AtCoder-Programming-Guide-for-beginners-APG4b-
-
-# URL
-
-https://atcoder.jp/contests/apg4b/tasks
